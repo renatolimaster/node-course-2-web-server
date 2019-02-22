62db5682da47b29e99710a792fcb04fa0b7ad7d0
renatolimaster <renatolimaster@gmail.com>
2019-02-22 13:09:00 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(`${now}`);
+  fs.appendFile('server.log', log + '\n', error => {
+    if (error) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// USE WITHOUT NEX() TO PREVENT TO EXECUTE THE APP IF SOMETHING GOES WRONG
+/* app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+}); */
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('geteCurrentYear', () => {
+  return new Date().getUTCFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express</h1>');
+  res.send({
+    name: 'Renato',
+    likes: ['Biking', 'Cities']
+  });
+});
+
+app.get('/home', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    wellcome: 'Welcome on Some Website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
